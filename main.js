387a300ec9709c0c46dc505343d600b62bcfa7b8
Friska Adisti Mahardini <friskamahardini@gmail.com>
2023-02-21 16:57:02 +0700

    update home-img slider

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-const homeImg = document.querySelector('.home-img');
-const images = ['images/skz.png', 'images/ive.png', 'images/treasure.png'];
-let currentImageIndex = 0;
-  
-setInterval(() => {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    homeImg.innerHTML = `<img src="${images[currentImageIndex]}" alt="">`;
-}, 3000);
+// navbar toggle
 
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
