161026d8f127a3a1f4788571d998826f06d4fc9e
Friska Adisti Mahardini <friskamahardini@gmail.com>
2023-02-22 20:42:06 +0700

    update home-img

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+const homeImg = document.querySelector('.img-container');
+const imgContainer = document.querySelectorAll('.img-container img');
+
+let counter = 0
+const size = imgContainer[0].clientWidth;
+  
+setInterval(() => {
+    console.log(homeImg);
+    homeImg.style.marginLeft = `-${0}00%`;
+    counter += 1;
+    console.log(counter);
+    if (counter === imgContainer.length) {
+        counter = 0;
+    }
+}, 1000);
